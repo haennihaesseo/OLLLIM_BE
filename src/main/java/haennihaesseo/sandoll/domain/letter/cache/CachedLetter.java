d5040d72c43a43ecb71e669cb8d2f932b69729a2
oosedus <seayeon0729@gmail.com>
2026-01-28 14:57:21 +0900

    feat: 캐쉬 저장용 편지 엔티티 및 레포지토리 생성

---
+++ src/main/java/haennihaesseo/sandoll/domain/letter/cache/CachedLetter.java
@@ -0,0 +1,42 @@
+package haennihaesseo.sandoll.domain.letter.cache;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@RedisHash(value = "letter")
+public class CachedLetter implements Serializable {
+
+    @Id
+    private String id;
+
+    private String voiceUrl;
+    private Integer duration;
+
+    @Builder.Default
+    private List<CachedWord> words = new ArrayList<>();
+
+    @TimeToLive
+    @Builder.Default
+    private Long ttl = 3600L;  // 1시간 (초 단위)
+
+    public void addWord(CachedWord word) {
+        this.words.add(word);
+    }
+
+    public void addWords(List<CachedWord> words) {
+        this.words.addAll(words);
+    }
+}
